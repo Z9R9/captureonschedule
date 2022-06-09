120ad11c33a80702166d86f8365f3947dc619c4b
Z9R9 <santoryukurozoro@protonmail.com>
2022-06-09 12:42:28 +0630

    updates: added scripts for killed and arrested

--- index.js
+++ index.js
@@ -48,8 +48,97 @@ async function captureScreenshot() {
         console.log(`❌ Error: ${err.message}`);
     } finally {
         await browser.close();
-        console.log(`\n🎉 Captured Graph `);
+        console.log(`\n🎉 Captured Graph \n\n`);
+    }
+}
+
+async function captureKilled() {
+    let browser = null;
+    try {
+        browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1000, height: 630 });
+        await page.goto("https://coup.aappb.org/capturekilled", { waitUntil: "networkidle2" });
+
+        async function screenshotDOMElement(selector, padding = 1) {
+            const rect = await page.evaluate((selector) => {
+                const element = document.querySelector(selector);
+                const { x, y, width, height } = element.getBoundingClientRect();
+                console.log("width", width);
+                console.log("height", height);
+                return {
+                    left: x,
+                    top: y,
+                    width: width,
+                    height: height,
+                    id: element.id,
+                };
+            }, selector);
+
+            return await page.screenshot({
+                path: path.join(__dirname, "./images/killed.png"),
+                clip: {
+                    x: rect.left - padding,
+                    y: rect.top - padding,
+                    width: rect.width + padding * 2,
+                    height: rect.height + padding * 2,
+                },
+            });
+        }
+
+        await screenshotDOMElement("#captureKilled", 120);
+    } catch (err) {
+        console.log(`❌ Error: ${err.message}`);
+    } finally {
+        await browser.close();
+        console.log(`\n🎉 Captured Killed Component \n\n`);
+    }
+}
+
+
+async function captureArrested() {
+    let browser = null;
+    try {
+        browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1000, height: 630 });
+        await page.goto("https://coup.aappb.org/capturearrested", { waitUntil: "networkidle2" });
+
+        async function screenshotDOMElement(selector, padding = 1) {
+            const rect = await page.evaluate((selector) => {
+                const element = document.querySelector(selector);
+                const { x, y, width, height } = element.getBoundingClientRect();
+                console.log("width", width);
+                console.log("height", height);
+                return {
+                    left: x,
+                    top: y,
+                    width: width,
+                    height: height,
+                    id: element.id,
+                };
+            }, selector);
+
+            return await page.screenshot({
+                path: path.join(__dirname, "./images/arrested.png"),
+                clip: {
+                    x: rect.left - padding,
+                    y: rect.top - padding,
+                    width: rect.width + padding * 2,
+                    height: rect.height + padding * 2,
+                },
+            });
+        }
+
+        await screenshotDOMElement("#captureArrested", 120);
+    } catch (err) {
+        console.log(`❌ Error: ${err.message}`);
+    } finally {
+        await browser.close();
+        console.log(`\n🎉 Captured Arrested Component \n\n`);
     }
 }
 
 captureScreenshot();
+captureKilled();
+captureArrested();
