57edb2be45e317d5ce406978a4b8054419865829
Z9R9 <santoryukurozoro@protonmail.com>
2022-07-13 12:35:42 +0630

    feat: avatar list, scripts update

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const fs = require("fs").promises;
 const puppeteer = require("puppeteer");
 const path = require("path");
 
-async function captureScreenshot() {
+async function captureImage(preferwidth, preferheight, preferpadding, element, url, name) {
     let browser = null;
     try {
         browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
-        await page.setViewport({ width: 1000, height: 630 });
-        await page.goto("https://coup.aappb.org/chart", { waitUntil: "networkidle2" });
-
+        await page.setViewport({ width: preferwidth || 415, height: preferheight || 750 });
+        await page.goto(url, { waitUntil: "networkidle2" });
         async function screenshotDOMElement(selector, padding = 1) {
             const rect = await page.evaluate((selector) => {
                 const element = document.querySelector(selector);
                 const { x, y, width, height } = element.getBoundingClientRect();
-                console.log("width", width);
-                console.log("height", height);
                 return {
                     left: x,
                     top: y,
@@ -24,9 +21,8 @@ async function captureScreenshot() {
                     id: element.id,
                 };
             }, selector);
-
             return await page.screenshot({
-                path: path.join(__dirname, "./images/preview.png"),
+                path: path.join(__dirname, `./images/${name}.png`),
                 clip: {
                     x: rect.left - padding,
                     y: rect.top - padding,
@@ -35,154 +31,19 @@ async function captureScreenshot() {
                 },
             });
         }
-
-        await screenshotDOMElement("#chart", 60);
-        const currentDate = new Date();
-        await fs.writeFile(
-            path.join(__dirname, "log.txt"),
-            `\n Updated graph preview on : ${currentDate.toString()}`,
-            "utf-8"
-        );
-        console.log("\n 🕟 Updated log file");
+        await screenshotDOMElement(element, preferpadding);
     } catch (err) {
         console.log(`❌ Error: ${err.message}`);
     } finally {
         await browser.close();
-        console.log(`\n🎉 Captured Graph \n\n`);
+        console.log(`\n🎉 Captured Mobile Graph: ${name} \n\n`);
     }
 }
 
-async function captureMobileScreenshot() {
-    let browser = null;
-    try {
-        browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
-        await page.setViewport({ width: 415, height: 750 });
-        await page.goto("https://coup.aappb.org/chart", { waitUntil: "networkidle2" });
-
-        async function screenshotDOMElement(selector, padding = 1) {
-            const rect = await page.evaluate((selector) => {
-                const element = document.querySelector(selector);
-                const { x, y, width, height } = element.getBoundingClientRect();
-                console.log("width", width);
-                console.log("height", height);
-                return {
-                    left: x,
-                    top: y,
-                    width: width,
-                    height: height,
-                    id: element.id,
-                };
-            }, selector);
-
-            return await page.screenshot({
-                path: path.join(__dirname, "./images/mobile-preview.png"),
-                clip: {
-                    x: rect.left - padding,
-                    y: rect.top - padding,
-                    width: rect.width + padding * 2,
-                    height: rect.height + padding * 2,
-                },
-            });
-        }
-
-        await screenshotDOMElement("#chart", 0);
-    } catch (err) {
-        console.log(`❌ Error: ${err.message}`);
-    } finally {
-        await browser.close();
-        console.log(`\n🎉 Captured Mobile Graph \n\n`);
-    }
-}
-
-async function captureKilled() {
-    let browser = null;
-    try {
-        browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1000, height: 630 });
-        await page.goto("https://coup.aappb.org/capturekilled", { waitUntil: "networkidle2" });
-
-        async function screenshotDOMElement(selector, padding = 1) {
-            const rect = await page.evaluate((selector) => {
-                const element = document.querySelector(selector);
-                const { x, y, width, height } = element.getBoundingClientRect();
-                console.log("width", width);
-                console.log("height", height);
-                return {
-                    left: x,
-                    top: y,
-                    width: width,
-                    height: height,
-                    id: element.id,
-                };
-            }, selector);
-
-            return await page.screenshot({
-                path: path.join(__dirname, "./images/killed.png"),
-                clip: {
-                    x: rect.left - padding,
-                    y: rect.top - padding,
-                    width: rect.width + padding * 2,
-                    height: rect.height + padding * 2,
-                },
-            });
-        }
-
-        await screenshotDOMElement("#captureKilled", 120);
-    } catch (err) {
-        console.log(`❌ Error: ${err.message}`);
-    } finally {
-        await browser.close();
-        console.log(`\n🎉 Captured Killed Component \n\n`);
-    }
-}
-
-
-async function captureArrested() {
-    let browser = null;
-    try {
-        browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1000, height: 630 });
-        await page.goto("https://coup.aappb.org/capturearrested", { waitUntil: "networkidle2" });
-
-        async function screenshotDOMElement(selector, padding = 1) {
-            const rect = await page.evaluate((selector) => {
-                const element = document.querySelector(selector);
-                const { x, y, width, height } = element.getBoundingClientRect();
-                console.log("width", width);
-                console.log("height", height);
-                return {
-                    left: x,
-                    top: y,
-                    width: width,
-                    height: height,
-                    id: element.id,
-                };
-            }, selector);
-
-            return await page.screenshot({
-                path: path.join(__dirname, "./images/arrested.png"),
-                clip: {
-                    x: rect.left - padding,
-                    y: rect.top - padding,
-                    width: rect.width + padding * 2,
-                    height: rect.height + padding * 2,
-                },
-            });
-        }
-
-        await screenshotDOMElement("#captureArrested", 120);
-    } catch (err) {
-        console.log(`❌ Error: ${err.message}`);
-    } finally {
-        await browser.close();
-        console.log(`\n🎉 Captured Arrested Component \n\n`);
-    }
-}
-
-captureScreenshot();
-captureMobileScreenshot();
-captureKilled();
-captureArrested();
+captureImage(1000, 630, 60, '#chart', 'https://coup.aappb.org/graph', 'preview');
+captureImage(415, 750, 0, '#chart', 'https://coup.aappb.org/graph', 'mobilegraph_en');
+captureImage(415, 750, 0, '#chart', 'https://coup.aappb.org/graph?language=my', 'mobilegraph_my');
+captureImage(1000, 630, 120, '#captureKilled', 'https://coup.aappb.org/capturekilled', 'killed_en');
+captureImage(1000, 630, 120, '#captureKilled', 'https://coup.aappb.org/capturekilled?language=my', 'killed_my');
+captureImage(1000, 630, 120, '#captureArrested', 'https://coup.aappb.org/captureArrested', 'arrested_en');
+captureImage(1000, 630, 120, '#captureArrested', 'https://coup.aappb.org/captureArrested?language=my', 'arrested_my');
